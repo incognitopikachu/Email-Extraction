675f2465b77eeab40dd7560bef6b7676fc9e19bd
SIMPSON Mark <msimpson@altranuk.local>
2019-09-10 15:53:21 +0100

    takes data from websites

--- email_dic.py
+++ email_dic.py
@@ -1,21 +1,33 @@
 import re
+from bs4 import BeautifulSoup
+import urllib.request
 
 f = open("sample.txt", 'r')
 file_text = f.read()
 f.close()
-# [A-Za-z0-9\.'%\+-]
-pattern = "\w+@(\w+(\.\w+)*)"
+
+link = "http://www.ex-parrot.com/pdw/Mail-RFC822-Address/Mail-RFC822-Address.html"
+page = urllib.request.urlopen(link)
+soup = BeautifulSoup(page, "html.parser")
+file_text = str(soup)
+# f = urllib.request.urlopen(link)
+# myfile = f.read()
+# file_text = myfile.decode("utf8")
+# print(myfile)
+
+pattern = "\w+@(\w+([\.|-]\w+)*)"
 domain_list = re.findall(pattern, file_text)
+print(domain_list)
+# domain_dic = {}
+dic = {}
 
-domain_dic = {}
-dic={}
-for domain in domain_list:
-    # print(domain[0])
-    if domain[0] in domain_dic:
-        domain_dic[domain[0]] = domain_dic[domain[0]] + 1
-
-    else:
-        domain_dic[domain[0]] = 1
+# for domain in domain_list:
+#     # print(domain[0])
+#     if domain[0] in domain_dic:
+#         domain_dic[domain[0]] = domain_dic[domain[0]] + 1
+#
+#     else:
+#         domain_dic[domain[0]] = 1
 
 
 
@@ -25,8 +37,8 @@ for domain in domain_list:
         dic[domain[0].split('.')[0]] = 1 + dic[domain[0].split('.')[0]]
     else:
         dic[domain[0].split('.')[0]] = 1
-print(dic)
-print(domain_dic)
+# print(dic)
+# print(domain_dic)
 
 def PrintMostCommon(mydict, count = 10, freq = 0):
     k = 0
@@ -39,6 +51,6 @@ def PrintMostCommon(mydict, count = 10, freq = 0):
 # print(domain_dic)
 # PrintMostCommon(domain_dic)
 
-# user_input = input("Minimum frequency: ")
-# freq = int(user_input)
-# PrintMostCommon(dic, len(domain_dic.keys()), freq)
+user_input = input("Minimum frequency: ")
+freq = int(user_input)
+PrintMostCommon(dic, len(dic.keys()), freq)
