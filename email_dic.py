4686019897c610c425676ec72e155825e0a6836c
SIMPSON Mark <msimpson@altranuk.local>
2019-09-10 12:29:13 +0100

    added user input

--- email_dic.py
+++ email_dic.py
@@ -3,7 +3,7 @@ import re
 f = open("sample.txt", 'r')
 file_text = f.read()
 f.close()
-
+# [A-Za-z0-9\.'%\+-]
 pattern = "\w+@(\w+(\.\w+)*)"
 domain_list = re.findall(pattern, file_text)
 
@@ -15,5 +15,16 @@ for domain in domain_list:
     else:
         domain_dic[domain[0]] = 1
 
+def PrintMostCommon(mydict, count = 10, freq = 0):
+    k = 0
+    for key, value in sorted(mydict.items(), key=lambda item: item[1], reverse = True):
+        if k < count and value > freq:
+            print("%s: %s" % (key, value))
+        k = k + 1
 
-print(domain_dic)
+# print(domain_dic)
+# PrintMostCommon(domain_dic)
+
+user_input = input("Minimum frequency: ")
+freq = int(user_input)
+PrintMostCommon(domain_dic, len(domain_dic.keys()), freq)
